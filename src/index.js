8d1515fce68de523d43224d9aaa62f8f99751f01
Vaylots <vaylots@gmail.com>
2022-08-18 11:52:07 +0200

    add a new feature

--- src/index.js
+++ src/index.js
@@ -18,6 +18,8 @@ const createWindow = () => {
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.setMenu(null)
+  mainWindow.setResizable(false)
 
 };
 
